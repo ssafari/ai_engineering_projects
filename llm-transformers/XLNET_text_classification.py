9f119113c6335a3f1ef6e96dcc494e677e04b224
Sepehr Safari <sepsafari@gmail.com>
2025-10-17 18:26:11 -0400

    LLM Transformers

--- llm-transformers/XLNET_text_classification.py
+++ llm-transformers/XLNET_text_classification.py
@@ -16,33 +16,26 @@ def preprocess_data(csv_file: str):
     data['text_clean'] = data['text'].apply(lambda x: clean(x, no_emoji=True))
     data['text_clean'] = data['text_clean'].apply(lambda x: re.sub('@[^\s]+', '', x))
     data.head(20)
-
     data['label'].value_counts().plot(kind="bar")
     g = data.groupby('label')
     data = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True)))
     data['label'].value_counts().plot(kind="bar")
-
     data['label_int'] = LabelEncoder().fit_transform(data['label'])
     train_split, test_split = train_test_split(data, train_size = 0.8)
     train_split, val_split = train_test_split(train_split, train_size = 0.9)
-
     print(len(train_split))
     print(len(test_split))
     print(len(val_split))
-
     train_df = pd.DataFrame({
         "label": train_split.label_int.values,
         "text": train_split.text_clean.values
     })
-
     test_df = pd.DataFrame({
         "label": test_split.label_int.values,
         "text": test_split.text_clean.values
     })
-
     train_df = datasets.Dataset.from_dict(train_df)
     test_df = datasets.Dataset.from_dict(test_df)
-
     dataset_dict = datasets.DatasetDict({"train":train_df, "test":test_df})
     print(dataset_dict)
     return dataset_dict
@@ -58,16 +51,12 @@ def create_embeddings():
     tokenizer = XLNetTokenizer.from_pretrained("xlnet-base-cased")
     tokenized_datasets = dataset_dict.map(tokenize_function, batched=True)
     print(tokenized_datasets)
-
     print(tokenized_datasets['train']['text'][0])
     print(tokenized_datasets['train']['input_ids'][0])
-
     print(tokenized_datasets['train']['token_type_ids'][0])
     print(tokenized_datasets['train']['attention_mask'][0])
-
     small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(100))
     small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(100))
-
     fine_tune_model(tokenizer, small_train_dataset, small_eval_dataset)
 
 
@@ -92,13 +81,9 @@ def fine_tune_model(tokenizer, small_train_dataset, small_eval_dataset):
         compute_metrics=compute_metrics)
 
     trainer.train()
-
     # Evaluate tuned model
     trainer.evaluate()
     model.save_pretrained("fine_tuned_model")
     fine_tuned_model = XLNetForSequenceClassification.from_pretrained("fine_tuned_model")
     clf = pipeline("text-classification", fine_tuned_model, tokenizer=tokenizer)
-    # rand_int = random.randint(0, len(val_split))
-    # print(val_split['text_clean'][rand_int])
-    # answer = clf(val_split['text_clean'][rand_int], top_k=None)
-    # print(answer)
+    
